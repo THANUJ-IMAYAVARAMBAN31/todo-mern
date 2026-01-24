9b86ae69fc5ca8e1aab68c39fc711531045b77e9
THANUJ-IMAYAVARAMBAN31 <varamban2007@gmail..com>
2026-01-24 19:44:55 +0530

    fix: add express.json middleware and prepare backend for Render

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ import todoRoutes from "./routes/todoRoutes.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 app.use("/api/todos", todoRoutes);
-app.use(express.json());
 const PORT = 4000;
 const MONGO_URI = process.env.MONGO_URI;
 
